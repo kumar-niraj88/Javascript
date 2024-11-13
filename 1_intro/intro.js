5b5cb92f7e584484f9b1ce5d5c3681fc11150e45
NIRAJ KUMAR <144611889+kumar-niraj88@users.noreply.github.com>
2024-11-13 16:34:36 +0530

    Add files via upload

---
+++ 1_intro/intro.js
@@ -0,0 +1,65 @@
+console.log("hello bhai hum extenal js hai")
+console.log("hai bhai kaise ho yaar")
+
+
+// Chariactricatics of js
+// High level language
+// Synchronous
+// Single Threaded
+// loosely typed
+// Dynamic 
+
+// how to declare the variable
+// let , var  const
+
+// Why dynamic 
+let n=10;
+n="Niraj";
+console.log(n);
+
+let a =10;
+var b = 20;
+const  c = 30;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// diff b/w  var  and let
+
+var b=200;   // its is possible to redeclare var keywords
+
+// let a=1000;   // it a not possible to redeclare to let keywords 
+
+
+
+var str 
+str ="hi am String";
+
+let str1
+str1 = "hi bhai hum let hai"
+
+
+// const str2      we cant iniliaz in other line  must be single line
+
+// str2 = "hi im const ";
+
+
+// Data types
+// Number
+// String
+// boolean
+// undefine 
+// null
+// bigInt
+
+// how to get output
+
+console.log("in console ");
+
+alert("heeloo im alert box");
+
+document.write("hello im document")
+
+
+
